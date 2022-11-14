0edd5cc4f5317718816a09f53eae0765f6984a0e
Juan Pablo Moya Alarcon <jmoya@unal.edu.co>
2022-11-14 17:16:10 -0500

    intento 1

--- pregunta.py
+++ pregunta.py
@@ -17,5 +17,43 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
-
+    filename = 'clusters_report.txt'
+    file = open(filename, mode='r')
+    titles1 = file.readline().split("  ")
+    for _ in range(3): file.readline()
+    titleTot = []
+    for t in titles1:
+        preTitle = t.strip()
+        if(preTitle != ''):
+            titleTot.append(preTitle)
+    titleTot[1] += " palabras clave"
+    titleTot[2] += " palabras clave"
+    titleTot = [t.replace(" ", "_").lower() for t in titleTot]
+    data = []
+    palabrasClave = ""
+    newRow = True
+    row = []
+    line = file.readline()
+    while(line):
+        if(newRow):
+            aux = line.strip().replace('%', '').replace('  ', ' ')
+            while('  ' in aux):
+                aux = aux.replace('  ',' ')
+            row = aux.split(' ')
+            newRow = False
+        elif(line.strip() == ''):
+            newRow = True
+            data.append([
+                int(row[0]),
+                int(row[1]),
+                float(row[2].replace(",",".")),
+                " ".join(row[3:])
+            ])
+        else:
+            aux = line.strip().replace(".","")
+            while('  ' in aux):
+                aux = aux.replace('  ',' ')
+            row += aux.split(' ')
+        line = file.readline()
+    df = pd.DataFrame(data, columns=titleTot)
     return df
